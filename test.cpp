0fcc3c3bb5844e419a736dcb5355552e14b41e80
Mi    tiepinuanhu <13236388+mi-tiepinuanhu@user.noreply.gitee.com>
2024-01-11 16:06:01 +0800

    version1

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 2e5+5;
+const int INF = 0x3f3f3f3f;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    return 0;
+}
